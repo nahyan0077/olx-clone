b2771544149fb3b44e578df1b492fde2ce3c15cf
nahyan0077 <nahyanmp4@gmail.com>
2024-07-18 15:40:24 +0530

    update file name to capitals

--- src/store/FirebaseContext.js
+++ src/store/FirebaseContext.js
@@ -1,5 +1,5 @@
 import {createContext, useState} from 'react'
-import {firestore} from '../firebase/config'
+import {firestore} from '../firebase/Config'
 
 
 export const FirebaseContext = createContext(null)
